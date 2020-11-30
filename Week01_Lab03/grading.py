f393106c4efd6fea3392c6860bc85353e802c41c
Lisa <brown.lisa999@gmail.com>
2020-11-30 15:57:10 -0800

    lab 3 version 1 added to repository

---
+++ Week01_Lab03/grading.py
@@ -0,0 +1,14 @@
+#get the grade from the user as an integer.
+grade = int(input("Please enter your grade as a number between 0 and 100: \n>"))
+
+#use if statements to convert the letter to a number
+if grade > 90:
+    print("You got an A!")
+elif grade > 80:
+    print ("You got a B!")
+elif grade > 70:
+    print ("You got a C!")
+elif grade > 60:
+    print ("You got a D!")
+else:
+    print ("You didn't pass this time.")
