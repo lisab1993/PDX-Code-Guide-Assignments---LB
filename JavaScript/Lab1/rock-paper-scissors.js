caee700667dbdee2bef33c0c8a8c474eef953d25
Lisa <brown.lisa999@gmail.com>
2021-01-13 22:07:37 -0800

    Rock Paper Scissors

--- JavaScript/Lab1/rock-paper-scissors.js
+++ JavaScript/Lab1/rock-paper-scissors.js
@@ -1,48 +1,79 @@
+//title at top
+//something saying to choose 
+//three pictures with the options
+//click on one to choose
+//after clicking, the program will run
+//the div output will have a picture for either victory or defeat, and will announce one or the other.
+
+//connect to our html
+let rock = document.querySelector("#rock")
+let paper = document.querySelector("#paper")
+let scissors = document.querySelector("#scissors")
+let div_output = document.querySelector('#div_output')
+
+// let selection = ''
+//alternate way to write functions
+const selectRock = function () {
+    let selection = 'rock'
+    rps(selection)
+}
+const selectPaper = function () {
+    let selection = 'paper'
+    rps(selection)
+}
+const selectScissors = function () {
+    let selection = 'scissors'
+    rps(selection)
+}
 
 
-while (true) {
-    let choices = ["rock", "paper", "scissors"]// an array of options for the computer to choose from.
-    let computerChoice = Math.floor((Math.random() * 3) + 0)// select a random number between 0 and two. The selection is now attached to the computerChoice variable.
-    computerChoice = (choices[computerChoice]) // the computerChoice variable will now use the random number as an index, and an option from the choices array will be held in this variable.
+const rps = function (selection) {
+    // while (true) {
+        let choices = ["rock", "paper", "scissors"]// an array of options for the computer to choose from.
+        let computerChoice = Math.floor((Math.random() * 3) + 0)// select a random number between 0 and two. The selection is now attached to the computerChoice variable.
+        computerChoice = choices[computerChoice] // the computerChoice variable will now use the random number as an index, and an option from the choices array will be held in this variable.
+       
+        // console.log(selection, computerChoice, "this is selection and computerChoice")
 
-    alert("Welcome to rock paper scissors!")// welcome screen
-    let playGame = prompt("Would you like to play rock, paper, scissors against the computer? Please enter yes or no:")// ask the user if they want to play; this will be used in our REPL.
-
-
-
-    if (playGame === "yes") {// see if the user wants to play. When they say "no", the REPL will break. Most of the game will be within these curly brackets because it will all run if the user says "yes".
-        let userInput = prompt("Please select either rock, paper, or scissors: ")// the user's choice is now stored within userInput
-
-        if (userInput === computerChoice) {// look for a tie.
-            alert("It's a tie!!!")
+        //a switch also would work; look it up later. javascript switch case
+        if (selection === computerChoice) { // look for a tie.
+            div_output.innerText = "It is a tie"
         }
 
-        // If the user has rock
-        else if (userInput === "rock" && computerChoice === "paper") {
-            alert("Paper covers rock. The computer wins!")
+        // If it's a tie
+        else if (selection === "rock" && computerChoice === "paper") {
+            div_output.innerText =  "Paper covers rock. The computer wins."
         }
-        else if (userInput === "rock" && computerChoice === "scissors") {
-            alert("Rock smashes scissors. You win!")
+        else if (selection === "rock" && computerChoice === "scissors") {
+            div_output.innerText =  "Rock smashes scissors. You win."
         }
 
         // If the user has paper
-        else if (userInput === "paper" && computerChoice === "scissors") {
-            alert("Scissors cuts paper. The computer wins!")
+        else if (selection === "paper" && computerChoice === "scissors") {
+            div_output.innerText =  "Scissors cuts paper. The computer wins."
         }
-        else if (userInput === "paper" && computerChoice === "rock") {
-            alert("Paper covers rock. You win!")
+        else if (selection === "paper" && computerChoice === "rock") {
+            div_output.innerText =  "Paper covers rock. You win."
         }
 
         // If the user has scissors
-        else if (userInput === "scissors" && computerChoice === "rock") {
-            alert("Rock smashes scissors. The computer wins!")
+        else if (selection === "scissors" && computerChoice === "rock") {
+            div_output.innerText =  "Rock smashes scissors. The computer wins."
         }
-        else if (userInput === "scissors" && computerChoice === "paper") {
-            alert("Scissors cuts paper. You win!")
+        else if (selection === "scissors" && computerChoice === "paper") {
+            div_output.innerText =  "Scissors cuts paper. You win."
         }
-    }
-    else {// If the user writes anything other than "yes" when asked if they want to play, we'll say goodbye to them. 
-        alert("Goodbye")
-        break// This will break the loop after one of the comparison statements runs.
-    }
+        else {
+            div_output.innerText = undefined
+        }
+    // }
 }
+
+
+
+
+// let input_starting_units = document.querySelector('#input_starting_units')
+// let input_ending_units = document.querySelector('#input_ending_units')
+// let input_distance = document.querySelector('#input_distance')
+// let btn_go = document.querySelector('#btn_go')
+// let div_output = document.querySelector('#div_output')
