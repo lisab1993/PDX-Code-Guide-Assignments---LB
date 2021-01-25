91b5b78695b4e057c8ad3e8f2817a9e447590cb5
Lisa <brown.lisa999@gmail.com>
2021-01-25 15:35:55 -0800

    Updated calculator lab

--- JavaScript/Lab6/calculator.js
+++ JavaScript/Lab6/calculator.js
@@ -1,6 +1,4 @@
-/*
- * when you click a number button, the calculate function is ran with your option as the first number.
- */
+
 
 let app = new Vue({
     el: "#app",
@@ -15,37 +13,57 @@ let app = new Vue({
     },
     methods: {
         calculator: function (calc_num) {
+            //all numbers will run this function when pressed so larger numbers can be generated.
             this.visible_output += calc_num
             this.holding += calc_num
-            console.log(this.holding)
+            this.output = ""
         }, math_operator: function (operation) {
+            //push the full first number into its slot; clear the holding variable; grab the operator entered.
             this.visible_output += operation
             this.first_number = this.holding
-            this.holding = ""
+            this.holding = ""//holding slot resets after entering an operator
             this.operator = operation
-            console.log(this.output)
         }, equals: function () {
+            // run the math on the numbers
             this.second_number = this.holding
+            this.holding = ""
             if (this.operator === "+") {
                 this.output = parseFloat(this.first_number) + parseFloat(this.second_number)
+                this.visible_output = ""
             } else if (this.operator === "-") {
                 this.output = parseFloat(this.first_number) - parseFloat(this.second_number)
+                this.visible_output = ""
             } else if (this.operator === "*") {
                 this.output = parseFloat(this.first_number) * parseFloat(this.second_number)
+                this.visible_output = ""
             } else if (this.operator === "/") {
                 this.output = parseFloat(this.first_number) / parseFloat(this.second_number)
+                this.visible_output = ""
+            } else if (this.operator === " modulus ") {
+                this.output = parseFloat(this.first_number) % parseFloat(this.second_number)
+                this.visible_output = ""
             } else if (this.operator === "^") {
                 let carat_hold = this.first_number //the local variable will copy the first number 
                 for (let i=1; i < this.second_number; ++i) {// we will keep run the loop the same number of times as our second number 
                     carat_hold = carat_hold * this.first_number// carat_hold will accumulate the total, and this.first_number will do the multiplication to add to it.
                 }
                 this.output = carat_hold
+                this.visible_output = ""
             }
-        }, backspace: function () {
-            this.holding = this.holding.substring(0, this.holding.length - 1)
-            this.visible_output = this.visible_output.substring(0, this.visible_output.length - 1)
-        }
-    }
+        }, backspace: function() {
+            // deletes the last character entered
+            this.holding = this.holding.substring(0, this.holding.length - 1)//this accesses the numbers inside the holding variable, and removes the last number or operator entered.
+            this.visible_output = this.visible_output.substring(0, this.visible_output.length - 1)//this does that for the visual-only output
+        }, clearall: function() {
+            // clears all characters entered by the user
+            this.holding  = ""
+            this.first_number = "",
+            this.operator = "",
+            this.second_number = "",
+            this.output = "",
+            this.visible_output = ""
+        },
+    }// <- closes the methods bracket; do not delete below this line.
 })
 
 
