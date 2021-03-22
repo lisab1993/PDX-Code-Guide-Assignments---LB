55249f3512779795c06393db96c14868c24c1012
Lisa <brown.lisa999@gmail.com>
2021-03-22 13:15:07 -0700

    Cleaned up

--- Fretboard_Project/fretboard.js
+++
@@ -1,7 +0,0 @@
-let input_tuning = document.querySelector('#input_tuning')
-let btn_go = document.querySelector('#btn_go')
-
-let alphabet = ["A", "Bb", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-
-
-
