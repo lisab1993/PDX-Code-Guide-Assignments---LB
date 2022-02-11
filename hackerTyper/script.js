2cf6c4baeb936e49beef65c965594e750c030772
Lisa <brown.lisa999@gmail.com>
2022-02-11 09:59:00 -0800

    color picker default set

--- hackerTyper/script.js
+++ hackerTyper/script.js
@@ -14,6 +14,10 @@ let fontCol = document.querySelector('#font-col')
 let theBody = document.getElementsByTagName('BODY')[0]
 let theArea = document.querySelector('#text-input')
 
+let fontSize = document.querySelector('#font-size')
+
+
+
 bgCol.addEventListener('input', (event) => {
     event.preventDefault()
     theBody.style.backgroundColor = bgCol.value
@@ -38,7 +42,7 @@ settingsBtn.addEventListener('click', (event) => {
     event.preventDefault()
     let settings = window.getComputedStyle(popup)
     settings = settings.display
-    if ( settings === 'none'){
+    if (settings === 'none') {
         popup.style.display = 'flex'
     } else {
         popup.style.display = 'none'
@@ -48,25 +52,13 @@ settingsBtn.addEventListener('click', (event) => {
 textInput.addEventListener('input', (event) => {
     //replace incoming text with hacker text from array
     event.preventDefault()
-    if (counter != condArr.length){
+    if (counter != condArr.length) {
         textInput.value = outputString += condArr[counter]
         counter += 1
     } else {
         counter = 0
     }
 })
-
-
-//ascii
-//control characters: 0-31
-//lowercase letters: 97-122
-//uppercase letters: 65-90
-//punctuations: 123-127; 91-96; 58-64; 32-47
-//numbers: 48-57
-//32 is a whitespace; 127 is delete
-//10 is new line
-//9 is tab
-
 
 
 function finder(target, lower, upper) {
@@ -82,10 +74,10 @@ function condensor(target) {
     let newLineCounter = 0
     for (i in textArr) {
         let word = textArr[i]
-        if (word === target && textArr[i+1] === target){
+        if (word === target && textArr[i + 1] === target) {
             newLineCounter += 1
-        } else if (word === target && textArr[i+1] !== target) {
-            textArr.push(target.repeat(newLineCounter+1))
+        } else if (word === target && textArr[i + 1] !== target) {
+            textArr.push(target.repeat(newLineCounter + 1))
             newLineCounter = 0
         } else {
             condArr.push(word)
@@ -102,11 +94,11 @@ function separator(callback) {
     for (i in textInputVal) {
         let letter = textInputVal[i]
         let textAscii = (textInputVal.charCodeAt(i))
-        if (finder(textAscii, 97, 122)){
+        if (finder(textAscii, 97, 122)) {
             word = word + letter
-        } else if (finder(textAscii, 65, 90)){
+        } else if (finder(textAscii, 65, 90)) {
             word = word + letter
-        }else {
+        } else {
             // console.grou(letter, 'false')
             textArr.push(word += letter)
             word = ''
@@ -116,10 +108,64 @@ function separator(callback) {
     callback(' ')
 }
 
+
+fontSize.addEventListener('input', (event) => {
+    event.preventDefault()
+    console.log('change')
+})
+
+
+
+function getColor(element) {
+    //give it an element, and it'll get the rgb value as a list of numbers
+    let startColor = (getComputedStyle(element).getPropertyValue('background-color'))
+    startColor = (startColor.slice(4, startColor.length - 1))
+
+    startColor = startColor.split(', ')
+    let outputArr = startColor.map(num => parseInt(num))
+    return outputArr
+}
+
+
+function ColorToHex(color) {
+    var hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+}
+
+function ConvertRGBtoHex(array) {
+    //turns a list of numbers into a hex code
+
+    return "#" + ColorToHex(array[0]) + ColorToHex(array[1]) + ColorToHex(array[2]);
+}
+
+
 function loadup() {
     //text from the html is grabbed for later use, and cleared so the user doesn't immediately see it
     textInput.innerHTML = ''
 
     //separator will use condensor as a callback function
     separator(condensor)
-  }
+
+    //populate the options in the font size drop down
+    for (i = 7; i <= 48; i++) {
+        let option = document.createElement('option')
+        option.setAttribute('value', i)
+        option.innerText = i
+        fontSize.appendChild(option)
+    }
+
+    bgCol.value = ConvertRGBtoHex(getColor(theBody))
+    taCol.value = ConvertRGBtoHex(getColor(theArea))
+    // fontCol.value = ConvertRGBtoHex(getColor())
+}
+
+
+//ascii
+//control characters: 0-31
+//lowercase letters: 97-122
+//uppercase letters: 65-90
+//punctuations: 123-127; 91-96; 58-64; 32-47
+//numbers: 48-57
+//32 is a whitespace; 127 is delete
+//10 is new line
+//9 is tab
